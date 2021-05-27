31b1e686905fcea903ce5832986eae9842c65c5a
velesov7493 <velesov7493@yandex.ru>
2021-05-27 12:35:31 +0000

    Новый тест.

--- src/test/java/TriggerTest.java
+++ src/test/java/TriggerTest.java
@@ -5,8 +5,13 @@ import static org.junit.Assert.*;
 public class TriggerTest {
 
     @Test
-    public void logicTest() {
+    public void logicSuccess() {
         assertEquals(1, Trigger.logic());
+    }
+
+    @Test
+    public void logicFail() {
+        assertEquals(0, Trigger.logic());
     }
 
 }
