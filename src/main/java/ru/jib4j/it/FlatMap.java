1965f544620f737c49351096781b18f42c032086
velesov7493 <velesov7493@yandex.ru>
2021-05-28 18:25:34 +0000

    5.1.4. FlatMap для Iterator<Iterator> [#152]

---
+++ src/main/java/ru/jib4j/it/FlatMap.java
@@ -0,0 +1,34 @@
+package ru.jib4j.it;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class FlatMap<T> implements Iterator<T> {
+
+    private final Iterator<Iterator<T>> data;
+    private Iterator<T> cursor;
+
+    public FlatMap(Iterator<Iterator<T>> data) {
+        this.data = data;
+        cursor = data.hasNext() ? data.next() : Collections.emptyIterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean result = cursor.hasNext();
+        while (!result && data.hasNext()) {
+            cursor = data.next();
+            result = cursor.hasNext();
+        }
+        return result;
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return cursor.next();
+    }
+}
