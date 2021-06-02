5e27a13718461bcf76c6541efd7d7d083896f8bc
velesov7493 <velesov7493@yandex.ru>
2021-06-02 13:55:10 +0000

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/list/ForwardLinked.java
@@ -0,0 +1,70 @@
+package ru.job4j.list;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ForwardLinked<T> implements Iterable<T> {
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    final class ElementsIterator implements Iterator<T> {
+
+        private Node<T> current;
+
+        public ElementsIterator() {
+            current = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T value = current.value;
+            current = current.next;
+            return value;
+        }
+    }
+
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> node = new Node<>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        T result = head.value;
+        head = head.next;
+        return result;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ElementsIterator();
+    }
+}
---
+++ src/test/java/ru/job4j/list/ForwardLinkedTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class ForwardLinkedTest {
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteFirst() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.deleteFirst();
+        linked.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteEmptyLinked() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.deleteFirst();
+    }
+
+    @Test
+    public void whenMultiDelete() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        assertThat(linked.deleteFirst(), is(1));
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+    }
+
+}
