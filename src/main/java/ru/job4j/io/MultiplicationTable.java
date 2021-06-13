f9d3b9ab4e85e1442041337b785e3674b5af55d7
velesov7493 <velesov7493@yandex.ru>
2021-06-13 06:03:05 +0000

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/MultiplicationTable.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("MultipleTable.txt")) {
+            for (int i = 1; i < 10; i++) {
+                String line = i + ":";
+                for (int j = 1; j < 10; j++) {
+                    int value = i * j;
+                    line += "\t" + value;
+                }
+                line += "\r\n";
+                out.write(line.getBytes());
+            }
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+    }
+}
