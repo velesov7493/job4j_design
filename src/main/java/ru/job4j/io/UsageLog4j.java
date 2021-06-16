3d863656f529db660507f944f173a5c314208e4a
velesov7493 <velesov7493@yandex.ru>
2021-06-16 05:24:55 +0000

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,18 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte dayOfMonth = 16;
+        short month = 6;
+        int year = 2021;
+        char hemisphere = 'N';
+        double latitude = 56.70035;
+        float longitude = 41.341F;
+        boolean active = true;
+        long timeStamp = System.currentTimeMillis();
+        LOG.debug(
+                "Gps dayOfMonth : {}, month : {}, year: {}, hemisphere: {}, latitude: {},"
+                + " longitude: {}, active: {}, timestamp: {}",
+                dayOfMonth, month, year, hemisphere, latitude, longitude, active, timeStamp
+        );
     }
 }
