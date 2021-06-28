d179a57a610a3f9688490523e102a2de3fc98a0e
velesov7493 <velesov7493@yandex.ru>
2021-06-28 16:02:35 +0000

    2. SQL Select by company [#2760]

---
+++ wiki/sql/selectByCompany.sql
@@ -0,0 +1,38 @@
+CREATE TABLE company
+(
+    id integer NOT NULL,
+    name character varying,
+    CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE person
+(
+    id integer NOT NULL,
+    name character varying,
+    company_id integer references company(id),
+    CONSTRAINT person_pkey PRIMARY KEY (id)
+);
+
+/*
+1. В одном запросе получить:
+- имена всех person, которые не состоят в компании с id = 5;
+- название компании для каждого человека.
+*/
+
+SELECT p.name, c.name
+FROM person AS p INNER JOIN company AS c ON
+    p.company_id=c.id AND
+    c.id <> 5
+ORDER BY 1,2;
+
+/*
+2. Необходимо выбрать название компании с максимальным количеством человек
++ количество человек в этой компании.
+*/
+
+SELECT c.name, COUNT(p.id)
+FROM person AS p INNER JOIN company AS c ON
+    p.company_id=c.id
+GROUP BY c.name
+ORDER BY 2 DESC
+LIMIT 1;
