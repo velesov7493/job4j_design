5d917eb5079749841ba4062fdea1fb8645530d2c
velesov7493 <velesov7493@yandex.ru>
2021-07-04 07:23:24 +0000

    0. Видео [#6853]

--- src/main/java/ru/job4j/gc/GcTypesDemo.java
+++ src/main/java/ru/job4j/gc/GcTypesDemo.java
@@ -7,13 +7,9 @@ public class GcTypesDemo {
     public static void main(String[] args) {
         Random random = new Random();
         int length = 100;
-        int j = 0;
         String[] data = new String[1000000];
-        for (int i = 0; j < length; i = (i + 1) % data.length) {
+        for (int i = 0; ; i = (i + 1) % data.length) {
             data[i] = String.valueOf((char) random.nextInt(255)).repeat(length);
-            if (i == data.length - 1) {
-                j++;
-            }
         }
     }
 }
