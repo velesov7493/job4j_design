cd4bac69e7028545ec6453175206c448d92530ac
velesov7493 <velesov7493@yandex.ru>
2021-07-07 06:38:19 +0000

    Реализации кеша на SoftReference [#1592 #174948]

--- src/main/java/ru/job4j/gc/cache/AbstractCache.java
+++ src/main/java/ru/job4j/gc/cache/AbstractCache.java
@@ -12,16 +12,26 @@ public abstract class AbstractCache<K, V> {
         cache = new HashMap<>();
     }
 
-    public V get(K key) {
-        SoftReference<V> result = cache.get(key);
-        if (result == null || result.get() == null) {
-            V value = load(key);
-            if (value != null) {
-                cache.put(key, new SoftReference<>(value));
-            }
-            return value;
+    private V updateReference(K key) {
+        V result = load(key);
+        if (result != null) {
+            cache.put(key, new SoftReference<>(result));
         }
-        return result.get();
+        return result;
+    }
+
+    public V get(K key) {
+        SoftReference<V> ref = cache.get(key);
+        V value;
+        if (ref == null) {
+            value = updateReference(key);
+        } else {
+            value = ref.get();
+            if (value == null) {
+                value = updateReference(key);
+            }
+        }
+        return value;
     }
 
     protected abstract V load(K key);
