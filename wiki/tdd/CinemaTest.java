5eee927c347b04603b6c2c81862fc38dfe40eb9e
velesov7493 <velesov7493@yandex.ru>
2021-07-10 14:52:51 +0000

    2. Что такое TDD? [#4918]

--- wiki/tdd/CinemaTest.java
+++ wiki/tdd/CinemaTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class CinemaTest {
 
     @Test
-    public void buy() {
+    public void whenBuySuccess() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
@@ -22,7 +22,19 @@ public class CinemaTest {
     }
 
     @Test
-    public void find() {
+    public void whenBuyFail() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket1 = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+        assertThat(ticket1, is(new Ticket3D()));
+        assertNull(ticket2);
+    }
+
+    @Test
+    public void whenFindSuccess() {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
@@ -30,7 +42,16 @@ public class CinemaTest {
     }
 
     @Test
-    public void add() {
+    public void whenFindFail() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> false);
+        List<Session> expected = new ArrayList();
+        assertThat(sessions, is(expected));
+    }
+
+    @Test
+    public void whenAdd() {
         Cinema cinema = new Cinema3D();
         Session3D expected = new Session3D();
         cinema.add(expected);
