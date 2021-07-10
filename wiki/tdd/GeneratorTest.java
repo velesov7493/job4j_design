65d6a4b3b289b851deb35d62dc84e9844b3e9573
velesov7493 <velesov7493@yandex.ru>
2021-07-10 16:14:54 +0000

    3. Шаблонизатор. [#855]

---
+++ wiki/tdd/GeneratorTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.tdd;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+public class GeneratorTest() {
+
+    @Test
+    public void whenProduceSuccess() {
+        String in = "I am a ${name}, Who are ${subject}?";
+        String expected = "I am a Petr Arsentev, Who are you?";
+        Generator g = new DefaultGenerator();
+        Map<String, String> replaces = new HashMap();
+        replaces.put("name", "Petr Arsentev");
+        replaces.put("subject", "you");
+        String result = g.produce(in, replaces);
+        assertEquals(expected, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenMissingReplace() {
+        String in = "I am a ${name}, Who are ${subject}?";
+        String expected = "I am a Petr Arsentev, Who are you?";
+        Generator g = new DefaultGenerator();
+        Map<String, String> replaces = new HashMap();
+        replaces.put("name", "Petr Arsentev");
+        String result = g.produce(in, replaces);
+        assertEquals(expected, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenMissingTemplateKey() {
+        String in = "I am a ${name}, Who are you?";
+        String expected = "I am a Petr Arsentev, Who are you?";
+        Generator g = new DefaultGenerator();
+        Map<String, String> replaces = new HashMap();
+        replaces.put("name", "Petr Arsentev");
+        replaces.put("subject", "you");
+        String result = g.produce(in, replaces);
+        assertEquals(expected, result);
+    }
+}
